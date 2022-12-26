e1274811dd64a652c5c3f40088c96da44daa2650
daljeet-coder <daljeetsingh.chamiyal@gmail.com>
2022-12-26 22:57:17 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
-      <h1>Hello world</h1>
+      <h1>Hello........</h1>
+      <h2>I am daljeet singh here</h2>
     </div>
   );
 }
